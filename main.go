b07730cbd9111e6f4a64a50ba8e7bdc1cbd8a889
aritra panda <pandaaritra76@gmail.com>
2026-02-28 03:00:57 +0530

    basic cli dome

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"time"
+)
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	completedAt *time.Time
+}
+
+type Todos []Todo
+
 func main() {
-	Todolist()
+	var todos Todos = make([]Todo, 0) // var todos = Todos{}
+
+	todos.add("make a todo app")
+	todos.add("new function added")
+	todos.add("drink tea")
+	todos.markeAsDone("drink tea")
+	todos.delete("new function added")
+	todos.listAll()
+
+	// fmt.Printf("%T \n", strconv.Itoa(56))
 }
--- todo.go
+++ todo.go
@@ -1,38 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
 
-type Todo struct {
-	Title     string
-	Completed bool
-	CreatedAt    time.Time
-	CompleatedAt *time.Time
-}
-
-type Todos []Todo
-
-func (todos *todos) add(title string){
+func (todos *Todos) add(title string) {
 	todo := Todo{
-		Title: title,
-		Completed: false,
-		CreatedAt: time.Now(),
+		Title:       title,
+		Completed:   false,
+		CreatedAt:   time.Now(),
+		completedAt: nil,
 	}
 
 	*todos = append(*todos, todo)
 }
 
+func (todos *Todos) listAll() {
+	for i, todo := range *todos {
+		fmt.Printf("%v :is -> %+v ", i, todo.Title)
+		if todo.Completed == true {
+			fmt.Printf("completed at %v", todo.completedAt.Format(time.RFC822))
+		}
+		fmt.Println()
 
+	}
+}
 
+func (todos *Todos) markeAsDone(title string) {
+	t := *todos
+	for i := range t {
+		if t[i].Title == title {
+			t[i].Completed = true
+			crrTime := time.Now()
+			t[i].completedAt = &crrTime
+		}
+	}
+}
 
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("invalid index")
+		return err
+	}
+	return nil
+}
 
-// func Todolist() {
-// 	list := make([]Todo, 5)
-
-// 	list[1] = Todo{Title: "get food", Completed: false}
-
-// 	fmt.Println(list[1])
-
-// }
+func (todos *Todos) delete(title string) {
+	deleted := false
+	for i, todo := range *todos {
+		if todo.Title == title {
+			*todos = append((*todos)[:i], (*todos)[i+1:]...)
+			deleted = true
+		}
+	}
+	if !deleted {
+		fmt.Printf("%v was not found \n", title)
+	}
+}
